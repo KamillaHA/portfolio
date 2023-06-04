9f3fa85287a1d7bdb2b02c046b9f766137cf3599
Kamilla <kaan0001@stud.kea.dk>
2023-06-04 12:41:33 +0200

    ny

---
+++ script.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    if (!link.classList.contains('active')) {
+      const active = document.querySelector('.link.active');
+      if (active !== null) {
+        active.classList.remove('active');
+      }
+      link.classList.add('active');
+    }
+  });
+});
