25669c46dbcd7b9c4384598d7611be91016273b1
Kamilla <kaan0001@stud.kea.dk>
2023-06-09 17:23:41 +0200

    efter alt slettet

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// const links = document.querySelectorAll('.link');
-
-// links.forEach(link => {
-//   link.addEventListener('click', e => {
-//     e.preventDefault();
-//     if (!link.classList.contains('active')) {
-//       const active = document.querySelector('.link.active');
-//       if (active !== null) {
-//         active.classList.remove('active');
-//       }
-//       link.classList.add('active');
-//       const href = link.getAttribute('href');
-//       window.location.href = href;
-//     }
-//   });
-// });
-
 
 // find knappen
 let mybutton = document.getElementById("myBtn");
