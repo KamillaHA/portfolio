b048a7cff3c296bc920211719096cc984a476335
Kamilla Huhnke <kaan0001@stud.kea.dk>
2023-12-18 13:50:31 +0100

    drft

--- script.js
+++ script.js
@@ -2,18 +2,20 @@
 let mybutton = document.getElementById("myBtn");
 
 // når bruger scroller over 20px ned på siden
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
 
 // når bruger klikker og skal til toppen igen
 function topFunction() {
-    document.body.scrollTop = 0; //til safari
-    document.documentElement.scrollTop = 0; // til chrome og firefox
+  document.body.scrollTop = 0; //til safari
+  document.documentElement.scrollTop = 0; // til chrome og firefox
 }
